1d2dfa562dda1e258764494f92c3fd62ccbcd36a
Humakt83 <urlact@yahoo.com>
2015-07-05 00:46:30 +0300

    Read ranged weapons from json

--- src/main/java/com/ukkosnetti/gladius/item/RangedWeapon.java
+++ src/main/java/com/ukkosnetti/gladius/item/RangedWeapon.java
@@ -1,9 +1,14 @@
 package com.ukkosnetti.gladius.item;
 
+import java.io.IOException;
 import java.io.Serializable;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+
+import com.google.gson.Gson;
 
 public class RangedWeapon implements WeaponInterface<RangedWeaponType>, Serializable {
 
@@ -48,7 +53,13 @@ public class RangedWeapon implements WeaponInterface<RangedWeaponType>, Serializ
 	}
 
 	public static List<RangedWeapon> getRangedWeapons() {
-		return Arrays.asList(new RangedWeapon(1, 3, RangedWeaponType.BOW, "Sling", 25), new RangedWeapon(2, 5, RangedWeaponType.BOW, "Bow", 50), new RangedWeapon(4, 7, RangedWeaponType.CROSSBOW,
+		try {
+			StringBuilder sb = new StringBuilder("");
+			Files.readAllLines(Paths.get("res/rangedweapons.json")).forEach(sb::append);
+			return Arrays.asList(new Gson().fromJson(sb.toString(), RangedWeapon[].class));
+		} catch (IOException ie) {
+			return Arrays.asList(new RangedWeapon(1, 3, RangedWeaponType.BOW, "Sling", 25), new RangedWeapon(2, 5, RangedWeaponType.BOW, "Bow", 50), new RangedWeapon(4, 7, RangedWeaponType.CROSSBOW,
 				"Crossbow", 75));
+		}
 	}
 }
